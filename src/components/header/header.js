9b032833a62d45a123fd07ff276d849e85eb8f44
aidanbeale <aidanbeale@gmail.com>
2023-06-02 16:01:21 +1000

    Add refresh button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import RefreshButton from "../refreshButton/refreshButton";
+
 import './header.css';
 
 const Sensor = () => {
   return (
     <div className="header-container">
       <h1 className="header-title">Sensor Dashboard</h1>
+      <RefreshButton />
     </div>
   )
 };
--- src/components/sensor/sensor.js
+++ src/components/sensor/sensor.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import './sensor.css';
 
 const Sensor = ({ sensorData }) => {
   let latestUpdate = sensorData.data[0];
-  const ts = new Date().getTime();
 
-  const currentTime = (ts-(ts%1000))/1000;
-  const lastUpdateTime = Math.floor((currentTime - latestUpdate.timestamp_TTL - 86400) / 1000);
+  const [minsSince, setMinsSince] = useState(0);
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 1000);
+  
+    const ts = new Date().getTime();
+    const currentTime = (ts-(ts%1000))/1000;
+    const lastUpdateTime = Math.floor((currentTime - (latestUpdate.timestamp_TTL - 86400)) / 60);
+    setMinsSince(lastUpdateTime);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [time]);
 
   return (
     <div className="sensor-container">
@@ -15,7 +27,7 @@ const Sensor = ({ sensorData }) => {
         <div className="status-indicator" />
         <div>
           <h2>{sensorData.name}</h2>
-          <p className="sensor-updated">Last Updated: {lastUpdateTime} minutes ago</p>
+          <p className="sensor-updated">Last Updated: {minsSince} minute{minsSince !== 1 ? 's' : ''} ago</p>
         </div>
       </div>
       <div className="stats-container">
