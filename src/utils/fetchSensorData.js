b080201830676b084a0159881ee4790979b83c3a
aidanbeale <aidanbeale@gmail.com>
2023-06-02 13:17:15 +1000

    Add fetch data method

---
+++ src/utils/fetchSensorData.js
@@ -0,0 +1,24 @@
+const fetchSensorData = (cities) => {
+  return new Promise((resolve, reject) => {
+    async function fetchData() {
+      const response = await fetch(process.env.REACT_APP_DATA_URL + '/data', {
+        method: "GET",
+      });
+      const responseData = await response.json();
+      if (responseData) {
+        return responseData;
+      }
+      throw Error("Nope. Try again.");
+    }
+
+    fetchData()
+      .then((fetchedData) => {
+        resolve(fetchedData);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
+
+export default fetchSensorData;
