9438813dbd79b2881613625a78dcb65c9442e57a
aidanbeale <aidanbeale@gmail.com>
2023-06-05 16:43:12 +1000

    Add popup box

---
+++ src/components/settings/settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './settings.css';
+
+const Settings = ({ setShowSettings }) => {
+  return (
+    <div className="settings-container">
+      <h1>Settings</h1>
+    </div>
+  );
+};
+
+export default Settings;
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 
 import Header from "../../components/header/header";
 import Sensor from '../../components/sensor/sensor';
+import Settings from '../../components/settings/settings';
 
 import './home.css';
 import fetchSensorData from "../../utils/fetchSensorData";
@@ -12,6 +13,8 @@ const Home = () => {
   const dispatch = useDispatch()
   const selectAppState = state => state.appState
   const appState = useSelector(selectAppState)
+
+  const [showSettings, setShowSettings] = useState(false);
 
   useEffect(() => {
     if (appState.rawSensorData.length === 0) {
@@ -27,8 +30,9 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <Header />
-      <div className="sensor-list-container">
+      <Header showSettings={showSettings} setShowSettings={setShowSettings} />
+      {showSettings ? <Settings setShowSettings={setShowSettings}/> : null}
+      <div className={"sensor-list-container" + (showSettings ? " blur" : "")}>
         {appState.filteredSensorData ? appState.filteredSensorData.map((sensorData, key) => {
           return <Sensor key={key} sensorData={sensorData} />
         }
